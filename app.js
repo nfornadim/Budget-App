c79ce24e084e37c063045ea82fbf33f4ade38c3a
Nadim <inspireze@gmail.com>
2018-12-14 19:24:31 +0530

    implementing module pattern

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
+var budgetController = (function() {
+	var x = 23;
+
+	var add = function(a) {
+		return x + a;
+	}
+
+	return {
+		publicTest: function(b) {
+			return add(b);
+		}
+	}
+
+})();
+
+
+var UIController = (function() {
+	// Some code
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+	var z = budgetCtrl.publicTest(8);
+
+	return {
+		anotherPublic: function() {
+			console.log(z);
+		}
+	}
+
+})(budgetController, UIController);
+	
+
+
+
+
+
+
+
+
+
+
+
+
 
