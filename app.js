3428024224b2bd140474f69515c03dbd29c3a794
Nadim <inspireze@gmail.com>
2018-12-14 22:07:06 +0530

    Creating an initialization Function

--- app.js
+++ app.js
@@ -35,7 +35,22 @@ var UIController = (function() {
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+
+		var DOM = UICtrl.getDOMstrings();
+	
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+
+			if (event.keyCode === 13 || event.which === 13) {
+				
+				ctrlAddItem();
+			}
+		});
+
+	};
+
 
 	var ctrlAddItem = function() {
 
@@ -53,20 +68,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 5. Display the budget on the UI
 	}
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-
-		if (event.keyCode === 13 || event.which === 13) {
-			
-			ctrlAddItem();
+	return {
+		init: function() {
+			setupEventListeners();
 		}
-	});
+	}
 
 })(budgetController, UIController);
 	
 
-
+controller.init();
 
 
 
