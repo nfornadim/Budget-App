ec393e2cca0de1c529d6405e0dcd20e40cdadb6b
Nadim <inspireze@gmail.com>
2018-12-16 14:49:47 +0530

    UX improved

--- app.js
+++ app.js
@@ -210,6 +210,12 @@ var UIController = (function() {
 			return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
 
 		};
+
+	var nodeListForEach = function(list, callback) {
+			for (var i = 0; i < list.length; i++) {
+				callback(list[i], i);
+			}
+	};
 	
 	return {
 		getInput: function() {
@@ -288,13 +294,6 @@ var UIController = (function() {
 
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			var nodeListForEach = function(list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
-
-
 			nodeListForEach(fields, function(current, index) {
 
 				if (percentages[index] > 0) {
@@ -320,6 +319,20 @@ var UIController = (function() {
 
 			year = now.getFullYear();
 			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
+
+		changedType: function() {
+
+			var fields = document.querySelectorAll(
+				DOMstrings.inputType + ',' +
+				DOMstrings.inputDescription + ',' +
+				DOMstrings.inputValue);
+
+			nodeListForEach(fields, function(cur) {
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 		},
 
 		getDOMstrings: function() {
@@ -348,6 +361,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
 	};
 
